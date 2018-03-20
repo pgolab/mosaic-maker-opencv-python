b329c1b29091bc58957b377b370ee00a50b8bbeb
Paweł Gołąb <pawel@millionmonkeys.pl>
2018-03-20 03:21:30 +0100

    better params

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import operator
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent
-IMAGES_SET = 'test'
-TARGET_IMAGE = 'test-target.jpg'
+IMAGES_SET = 'underwater'
+TARGET_IMAGE = 'nemo.jpg'
 
-PATCH_SIZE = 16
+PATCH_SIZE = 32
 
 SOBEL_BLUR_KERNEL_SHAPE = (3, 3)
 EDGES_LOWER_THRESHOLD = 120
@@ -16,7 +16,7 @@ HISTOGRAM_BUCKETS = (8, 8, 8)
 HISTOGRAM_VECTOR_LENGTH = functools.reduce(operator.mul, HISTOGRAM_GRID_SHAPE, 1) \
                           * functools.reduce(operator.mul, HISTOGRAM_BUCKETS, 1)
 
-MAX_ACCEPTED_MIN_HISTOGRAM_DISTANCE = 0.006
+MAX_ACCEPTED_MIN_HISTOGRAM_DISTANCE = 0.003
 
 USE_SOBEL_DESCRIPTOR = True
 USE_HISTOGRAM_DESCRIPTOR = True
